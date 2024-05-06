a95260d9dfc24876fbef9e2c896c8240c14f08f0
thodale <thomas@dalecompany.io>
2024-05-06 22:42:08 +0100

    Default macro amount to 0, require alcohol

--- CalorieTracker/src/Macro.cs
+++ CalorieTracker/src/Macro.cs
@@ -1,33 +1,29 @@
+using CalorieTracker.Utils;
+
 namespace CalorieTracker;
 
-public abstract class Macro(float amount)
-{
+public abstract class Macro(float amount = 0) {
     public abstract string Name { get; }
     public float Amount { get; set; } = amount;
     public abstract int CaloriesPerGram { get; }
 }
 
-public class Protein(float amount) : Macro(amount)
-{
+public class Protein(float amount = 0) : Macro(amount) {
     public override string Name => "Protein";
-    public override int CaloriesPerGram => 4;
+    public override int CaloriesPerGram => MacroUtils.ProteinCaloriesPerGram;
 }
 
-public class Carbohydrates(float amount) : Macro(amount)
-{
-    
+public class Carbohydrates(float amount = 0) : Macro(amount) {
     public override string Name => "Carbohydrates";
-    public override int CaloriesPerGram => 4;
+    public override int CaloriesPerGram => MacroUtils.CarbohydratesCaloriesPerGram;
 }
 
-public class Fat(float amount) : Macro(amount)
-{
+public class Fat(float amount = 0) : Macro(amount) {
     public override string Name => "Fat";
-    public override int CaloriesPerGram => 9;
+    public override int CaloriesPerGram => MacroUtils.FatCaloriesPerGram;
 }
 
-public class Alcohol(float amount) : Macro(amount)
-{
+public class Alcohol(float amount = 0) : Macro(amount) {
     public override string Name => "Alcohol";
-    public override int CaloriesPerGram => 7;
+    public override int CaloriesPerGram => MacroUtils.AlcoholCaloriesPerGram;
 }
