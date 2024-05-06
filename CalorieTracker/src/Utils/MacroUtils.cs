9f5be314db3ccf05a9a58b24e817468d85aec060
thodale <thomas@dalecompany.io>
2024-05-06 22:41:48 +0100

    Added MacroUtils

---
+++ CalorieTracker/src/Utils/MacroUtils.cs
@@ -0,0 +1,8 @@
+namespace CalorieTracker.Utils;
+
+public static class MacroUtils {
+    public readonly static int ProteinCaloriesPerGram = 4;
+    public readonly static int CarbohydratesCaloriesPerGram = 4;
+    public readonly static int FatCaloriesPerGram = 9;
+    public readonly static int AlcoholCaloriesPerGram = 7;
+}
