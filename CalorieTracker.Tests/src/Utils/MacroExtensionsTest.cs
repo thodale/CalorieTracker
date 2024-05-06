359e35cb40ebba0f174a17a674fea396c6f0e380
thodale <thomas@dalecompany.io>
2024-05-06 23:52:16 +0100

    Add MacroExtensionsTest

---
+++ CalorieTracker.Tests/src/Utils/MacroExtensionsTest.cs
@@ -0,0 +1,25 @@
+using CalorieTracker.Utils;
+using JetBrains.Annotations;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalorieTracker.Tests.Utils;
+
+[TestClass]
+[TestSubject(typeof(MacroExtensions))]
+public class MacroExtensionsTest {
+
+    [TestMethod]
+    public void TestTotalCalories() {
+        const int amount = 10;
+
+        var fat = new Fat(amount);
+        var alcohol = new Alcohol(amount);
+        var protein = new Protein(amount);
+        var carbohydrates = new Carbohydrates(amount);
+
+        Assert.AreEqual(40, protein.TotalCalories());
+        Assert.AreEqual(40, carbohydrates.TotalCalories());
+        Assert.AreEqual(70, alcohol.TotalCalories());
+        Assert.AreEqual(90, fat.TotalCalories());
+    }
+}
