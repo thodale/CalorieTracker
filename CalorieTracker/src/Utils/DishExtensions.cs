abcf5bd1558d0a111e61dc91b36fbdf485ec8973
thodale <thomas@dalecompany.io>
2024-05-06 22:42:45 +0100

    Return List of macros instead of IEnumerable

--- CalorieTracker/src/Utils/DishExtensions.cs
+++ CalorieTracker/src/Utils/DishExtensions.cs
@@ -1,14 +1,16 @@
 namespace CalorieTracker.Utils;
 
-public static class DishExtensions
-{
-    public static IEnumerable<Macro> GetMacros(this Dish dish)
-    {
-        return dish.Ingredients.SelectMany(i => new Macro[] { i.Protein, i.Carbohydrates, i.Fat, i.Alcohol });
+public static class DishExtensions {
+    public static List<Macro> GetMacros(this Dish dish) {
+        return dish.Ingredients.SelectMany(i => new Macro[] {
+            i.Protein,
+            i.Carbohydrates,
+            i.Fat,
+            i.Alcohol
+        }).ToList();
     }
-    
-    public static int TotalCalories(this Dish dish)
-    {
+
+    public static int TotalCalories(this Dish dish) {
         return dish.GetMacros().Sum(m => m.TotalCalories());
     }
 }
