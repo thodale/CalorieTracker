dc6a89732111f24c59eb39af7686acf2c87723f6
thodale <thomas@dalecompany.io>
2024-05-10 19:38:45 +0100

    Slight refactor to improve readability

--- CalorieTracker/src/Utils/NutritionUtils.cs
+++ CalorieTracker/src/Utils/NutritionUtils.cs
@@ -1,6 +1,7 @@
 namespace CalorieTracker.Utils;
 
 public static class NutritionUtils {
+    private const float AlcoholDensityInGrams = 0.789f;
 
     /// <summary>
     ///     Returns the amount of alcohol in grams from the given percentage and volume.
@@ -9,6 +10,6 @@ public static class NutritionUtils {
     /// <param name="volume">The amount of liquid in milliliters.</param>
     /// <returns></returns>
     public static float GetAlcoholAmountInGramsFromPercentage(float percentageAmount, float volume = 100) {
-        return volume * (percentageAmount / 100) * 0.789f;
+        return volume * (percentageAmount / 100) * AlcoholDensityInGrams;
     }
 }
