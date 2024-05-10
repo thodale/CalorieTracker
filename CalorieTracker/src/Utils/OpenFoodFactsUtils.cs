039b5f7ca2f3c59d61058738e4b492937e20a996
thodale <thomas@dalecompany.io>
2024-05-10 19:17:32 +0100

    Added OpenFoodFactsDatabase, OpenFoodFactsUtils

---
+++ CalorieTracker/src/Utils/OpenFoodFactsUtils.cs
@@ -0,0 +1,13 @@
+namespace CalorieTracker.Utils;
+
+public static class OpenFoodFactsUtils {
+    public const string NutrimentsPropertyName = "nutriments";
+    public const string ProductPropertyName = "product";
+    public const string ProductNamePropertyName = "product_name";
+    public const string ProductIdPropertyName = "_id";
+
+    public const string CarbohydratesPropertyName = "carbohydrates";
+    public const string ProteinPropertyName = "proteins";
+    public const string AlcoholPropertyName = "alcohol";
+    public const string FatPropertyName = "fat";
+}
